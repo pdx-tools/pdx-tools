d3060ecbb8dca37d3b26ca154d215e0a37b92ab7
Nick Babcock <nbabcock19@hotmail.com>
2022-01-25 17:34:58 -0600

    Update social image, title, and description
    Based on feedback from https://socialsharepreview.com/?url=https://pdx.tools
    I updated the image to be a slightly different view. It was pointed out
    that pie charts and bar charts are probably easier to distinguish
    compared to line charts when the image is small -- like it could be when
    shared.
    
    Title and description were slightly updated to match recommendations.

--- src/app/src/pages/index.tsx
+++ src/app/src/pages/index.tsx
@@ -8,10 +8,19 @@ export const IndexPage: React.FC<{}> = () => {
   return (
     <>
       <HtmlHead>
-        <title>PDX Tools</title>
+        <title>PDX Tools - Modern EU4 Save Analyzer</title>
         <meta
           name="description"
-          content="Modern EU4 save file analyzer. View maps, graphs, and tables of your save and compete in a casual leaderboard of EU4 achievement speed runs. Upload and share your save with the world."
+          content="View maps, graphs, and tables of your save and compete in a casual, evergreen leaderboard of EU4 achievement speed runs. Upload and share your save with the world."
+        ></meta>
+
+        <meta
+          name="og:title"
+          content="PDX Tools - Modern EU4 Save Analyzer"
+        ></meta>
+        <meta
+          name="og:description"
+          content="View maps, graphs, and tables of your save and compete in a casual, evergreen leaderboard of EU4 achievement speed runs. Upload and share your save with the world."
         ></meta>
       </HtmlHead>
       <AppStructure>
