d4966271788c66d03b91789d616142f48cd71637
Nick Babcock <nbabcock19@hotmail.com>
2022-01-26 16:25:28 -0600

    Rework home page and file analysis box
    As pointed out in #42, the file analysis box is filled with too much
    text. So I decided to sample the ecosystem and find inspiration.
    
    There's a common theme between all (or nearly all) of them:
    
    - Minimal text
    - Centered text
    - Lots of padding
    - Dashed border
    - Usage of the phrase "drag and drop"
    - An icon (!)
    - A button or obvious indicator to click
    
    So I tried to follow their cues the best I could and came up with this
    before / after:
    
    An interesting note is that the "browse" button isn't actually a button
    -- it is just stylized text. That's because the containing box with the
    dashed border is the "button", but I needed to give users a visual
    indicator that the box is clickable. No accessibility worries as it is
    only a visual indicator and carries no function itself.
    
    It's nice to see the [Nielsen Normal group agree][0] with the
    forethought I had to allow file drops outside drop target, as even when
    users don't make it to the drop target the file should still be
    analyzed.
    
    One missing feature is an icon. It would be nice to have an icon made
    kinda like the "xsl, xslx, csv" one except with eu4, ck3, hoi4 (with eu4
    most prominent). This would adequately solve #18 as well.
    
    This may have swung the pendulum too far in the other direction such
    that new users may not be given enough context.
    
    The home page has also been updated as part of this commit, which
    highlights a few more features.
    
    Closes #42
    
    [0]: https://www.nngroup.com/articles/drag-drop/

--- src/app/src/components/landing/AnalyzeBox.tsx
+++ src/app/src/components/landing/AnalyzeBox.tsx
@@ -33,11 +33,12 @@ export const AnalyzeBox: React.FC<{}> = () => {
       <label
         tabIndex={0}
         onKeyDown={keyboardTrigger(labelFocus)}
-        className={isFileHover ? "hover" : ""}
+        className={`flex-col text-center ${isFileHover ? "hover" : ""}`}
       >
-        Maps, graphs, and data await your EU4 save. Drag and drop a save onto
-        this page or select this box and analyze your save. PDX Tools is a
-        modern EU4 savefile analyzer for the browser. Upload and share.
+        <div>Analyze a EU4 save for maps, graphs, and data.</div>
+        <div>
+          Drag and drop or <span className="font-bold">browse</span>
+        </div>
         <input
           ref={fileInputRef}
           hidden
@@ -55,24 +56,35 @@ export const AnalyzeBox: React.FC<{}> = () => {
           border: 0;
           outline: 0.5rem dashed white;
           margin: 0.5rem;
-          padding: 1rem 2rem;
+          padding-block: clamp(1rem, 10%, 6rem);
+          padding-inline: clamp(1rem, 10%, 6rem);
           border-radius: 1rem;
           background-color: transparent;
           cursor: pointer;
-          display: inline-block;
-          font-size: 1.5rem;
-        }
-
-        @media screen and (min-width: 768px) {
-          label {
-            font-size: 2rem;
-          }
+          font-size: clamp(1.25rem, 3vw, 2rem);
+          gap: clamp(1.25rem, 3vw, 2rem);
         }
 
         .hover,
         label:hover,
         label:focus {
           outline: 0.5rem dashed rgb(var(--secondary-light));
+        }
+
+        span {
+          margin-inline-start: 0.5rem;
+
+          /* From antd: */
+          border-radius: 4px;
+          padding: 4px 15px;
+          border: 4px solid #d9d9d9;
+          transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+        }
+
+        span:hover,
+        span:focus {
+          border-color: rgb(var(--secondary-light));
+          color: rgb(var(--secondary-light));
         }
       `}</style>
     </div>
