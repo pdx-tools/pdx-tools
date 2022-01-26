98fefbd97808c48d387ed795a79274e5be12d376
Nick Babcock <nbabcock19@hotmail.com>
2022-01-25 19:49:09 -0600

    Use correct meta attributes for open graph

--- src/app/src/pages/index.tsx
+++ src/app/src/pages/index.tsx
@@ -15,11 +15,11 @@ export const IndexPage: React.FC<{}> = () => {
         ></meta>
 
         <meta
-          name="og:title"
+          property="og:title"
           content="PDX Tools - Modern EU4 Save Analyzer"
         ></meta>
         <meta
-          name="og:description"
+          property="og:description"
           content="View maps, graphs, and tables of your save and compete in a casual, evergreen leaderboard of EU4 achievement speed runs. Upload and share your save with the world."
         ></meta>
       </HtmlHead>
