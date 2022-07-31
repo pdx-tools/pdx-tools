2dfb02f84b07b441d3c2a7d23f25974537b269e4
Nick Babcock <nbabcock19@hotmail.com>
2022-07-31 16:30:04 +0000

    Fix side bar button styling on firefox

--- src/app/src/features/eu4/components/SideBarButton.tsx
+++ src/app/src/features/eu4/components/SideBarButton.tsx
@@ -18,7 +18,7 @@ export const SideBarButton = ({
   return (
     <button
       style={{ ...style, ...durationStyle }}
-      className={`${className} ${classes["slide-in"]} w-[60px] h-[60px] bg-rose-800 active:bg-rose-900 drop-shadow-md`}
+      className={`${className} ${classes["slide-in"]} w-[60px] h-[60px] bg-rose-800 active:bg-rose-900 border-1 border-solid border-black drop-shadow-md`}
       {...rest}
     >
       {children}
