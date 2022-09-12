df268a03872c448e36453a554709d65014df4202
Nick Babcock <nbabcock19@hotmail.com>
2022-09-12 11:58:31 +0000

    Correct eu4 achievement patch weights

--- src/eu4game/src/achievements.rs
+++ src/eu4game/src/achievements.rs
@@ -560,13 +560,10 @@ impl WeightedScore {
 }
 
 pub fn weighted_factor(major: u16, minor: u16) -> Option<f64> {
-    match (major, minor) {
-        (1, 29) => Some(1.4),
-        (1, 30) => Some(1.3),
-        (1, 31) => Some(1.2),
-        (1, 32) => Some(1.1),
-        (1, 33) => Some(1.0),
-        _ => None,
+    if major != 1 || minor < 29 {
+        None
+    } else {
+        Some(1.0 + f64::from(34 - minor.min(34)) * 0.1)
     }
 }
 
