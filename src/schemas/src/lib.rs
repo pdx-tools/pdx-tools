cb680abc2624a49d543c05729bd6dfb4d21c93bb
Nick Babcock <nbabcock19@hotmail.com>
2023-05-18 10:41:01 +0000

    Fix outdated flatbuffer token reference

--- src/schemas/src/lib.rs
+++ src/schemas/src/lib.rs
@@ -2,13 +2,8 @@
 #[path = "../target/flatbuffers/eu4_generated.rs"]
 mod eu4_flatbuffers;
 
-#[allow(non_snake_case, unused_imports, clippy::all)]
-#[path = "../target/flatbuffers/tokens_generated.rs"]
-mod tokens_flatbuffers;
-
 pub mod resolver;
 
 pub use eu4_flatbuffers::rakaly::eu_4 as eu4;
 pub use flatbuffers;
 pub use resolver::{FlatResolver, BREAKPOINT};
-pub use tokens_flatbuffers::rakaly::tokens;
