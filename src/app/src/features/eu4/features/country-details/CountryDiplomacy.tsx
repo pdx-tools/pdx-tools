2f08820a20a2b999fa65936d23bdb9a6acc1722e
Nick Babcock <nbabcock19@hotmail.com>
2023-07-20 03:09:19 +0000

    Round monthly subsidies

--- src/app/src/features/eu4/features/country-details/CountryDiplomacy.tsx
+++ src/app/src/features/eu4/features/country-details/CountryDiplomacy.tsx
@@ -350,7 +350,7 @@ export const CountryDiplomacy = ({ details }: { details: CountryDetails }) => {
           {(x) => (
             <div>
               <p className="m-0 text-sm">{`${x.name} (${x.tag})`}</p>
-              <p className="m-0 text-sm">Monthly amount: {x.amount}</p>
+              <p className="m-0 text-sm">Monthly amount: {formatInt(x.amount)}</p>
               {x.start_date && (
                 <p className="m-0 text-sm">{`Since ${x.start_date}${
                   x.total !== undefined && `: ${formatInt(x.total)}`
