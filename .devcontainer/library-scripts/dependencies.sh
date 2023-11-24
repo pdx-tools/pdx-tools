d2532109f75ec33773d9701796a9b1ac30a45efb
Nick Babcock <nbabcock19@hotmail.com>
2023-11-24 13:54:00 +0000

    Update development environment to latest
    The node 18 version that was previously used in the dev container was
    not supported by next.js, so the devcontainer was updated to target
    node.js 20 lts, so we should be good for awhile.

--- .devcontainer/library-scripts/dependencies.sh
+++ .devcontainer/library-scripts/dependencies.sh
@@ -4,12 +4,12 @@ set -euxo pipefail
 MY_TMP="$(mktemp -d)"
 trap 'rm -rf -- "$MY_TMP"' EXIT
 
-WASM_OPT_V=version_110
+WASM_OPT_V=version_116
 curl -o "$MY_TMP/binaryen.tar.gz" -L "https://github.com/WebAssembly/binaryen/releases/download/${WASM_OPT_V}/binaryen-${WASM_OPT_V}-x86_64-linux.tar.gz"
 (cd "$MY_TMP" && tar -xzf "binaryen.tar.gz")
 mv "$MY_TMP/binaryen-$WASM_OPT_V/bin/wasm-opt" "/usr/local/bin/."
 
-curl -o "$MY_TMP/flatc.zip" -L "https://github.com/google/flatbuffers/releases/download/v22.9.29/Linux.flatc.binary.clang++-12.zip"
+curl -o "$MY_TMP/flatc.zip" -L "https://github.com/google/flatbuffers/releases/download/v23.5.26/Linux.flatc.binary.clang++-12.zip"
 (cd "$MY_TMP" && unzip flatc.zip)
 chmod +x "$MY_TMP/flatc"
 mv "$MY_TMP/flatc" "/usr/local/bin/."
--- .devcontainer/library-scripts/npm-dependencies.sh
+++ .devcontainer/library-scripts/npm-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-npm install -g wasm-pack@0.10.1 vercel@32.1.0
+npm install -g wasm-pack@0.10.1 vercel@32.5.6
