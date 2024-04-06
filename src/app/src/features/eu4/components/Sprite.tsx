8b13262b40ba5b65190ff9511d15c05423beec4d
Nick Babcock <nbabcock19@hotmail.com>
2024-04-06 23:11:39 +0000

    Make sprite a forwardref for future lazy load

--- src/app/src/features/eu4/components/Sprite.tsx
+++ src/app/src/features/eu4/components/Sprite.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export function spriteDimension({ data }: { data: Record<string, number> }) {
   const total = Object.keys(data).length;
   const cols = Math.ceil(Math.sqrt(total));
@@ -5,35 +7,33 @@ export function spriteDimension({ data }: { data: Record<string, number> }) {
   return { cols, rows };
 }
 
-export function Sprite({
-  src,
-  index,
-  width,
-  height,
-  dimensions,
-  ariaLabel,
-}: {
+type SpriteProps = {
   src: string;
   index: number;
   width: number;
   height: number;
   dimensions: ReturnType<typeof spriteDimension>;
   ariaLabel?: string;
-}) {
-  const row = Math.floor(index / dimensions.cols);
-  const col = index % dimensions.cols;
+};
 
-  return (
-    <div
-      role={ariaLabel ? "img" : "presentation"}
-      aria-label={ariaLabel}
-      style={{
-        width,
-        height,
-        backgroundImage: `url(${src})`,
-        backgroundPosition: `-${col * width}px -${row * height}px`,
-        backgroundSize: `${dimensions.cols * 100}% ${dimensions.rows * 100}%`,
-      }}
-    />
-  );
-}
+export const Sprite = React.forwardRef<HTMLDivElement, SpriteProps>(
+  function Sprite({ src, index, width, height, dimensions, ariaLabel }, ref) {
+    const row = Math.floor(index / dimensions.cols);
+    const col = index % dimensions.cols;
+
+    return (
+      <div
+        ref={ref}
+        role={ariaLabel ? "img" : "presentation"}
+        aria-label={ariaLabel}
+        style={{
+          width,
+          height,
+          backgroundImage: `url(${src})`,
+          backgroundPosition: `-${col * width}px -${row * height}px`,
+          backgroundSize: `${dimensions.cols * 100}% ${dimensions.rows * 100}%`,
+        }}
+      />
+    );
+  }
+);
