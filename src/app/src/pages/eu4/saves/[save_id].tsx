06c315bb5dbc08b9b79d6eec312fed361f4ab96d
Nick Babcock <nbabcock19@hotmail.com>
2024-09-03 11:00:13 +0000

    Disconnect save page from og service
    OG images must be present at time of scrape to be effective. So the save
    page must be rendered on a server (edge or otherwise), but it's not
    possible to next.js web worker issues.

--- src/app/src/pages/eu4/saves/[save_id].tsx
+++ src/app/src/pages/eu4/saves/[save_id].tsx
@@ -12,11 +12,9 @@ export const Eu4Save = () => {
     return null;
   }
 
-  const addr = process.env.NEXT_PUBLIC_EXTERNAL_ADDRESS;
-  const social = addr ? `${addr}/api/saves/${save_id}/og` : undefined;
   return (
     <Root>
-      <HtmlHead social={social}>
+      <HtmlHead>
         <title>Loading Save: {save_id} - EU4 - PDX Tools</title>
       </HtmlHead>
       <SavePage saveId={save_id} />
