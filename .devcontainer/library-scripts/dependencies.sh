916dafc2130a31d7988cf543d76498f9bdce1994
Nick Babcock <nbabcock19@hotmail.com>
2024-11-22 17:04:21 -0600

    Support pdx tools development on arm macs

--- .devcontainer/library-scripts/dependencies.sh
+++ .devcontainer/library-scripts/dependencies.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 MY_TMP="$(mktemp -d)"
 trap 'rm -rf -- "$MY_TMP"' EXIT
 
-WASM_OPT_V=version_120
-curl -o "$MY_TMP/binaryen.tar.gz" -L "https://github.com/WebAssembly/binaryen/releases/download/${WASM_OPT_V}/binaryen-${WASM_OPT_V}-x86_64-linux.tar.gz"
+WASM_OPT_V=version_120_b
+curl -o "$MY_TMP/binaryen.tar.gz" -L "https://github.com/WebAssembly/binaryen/releases/download/${WASM_OPT_V}/binaryen-${WASM_OPT_V}-$(uname -m)-linux.tar.gz"
 (cd "$MY_TMP" && tar -xzf "binaryen.tar.gz")
 mv "$MY_TMP/binaryen-$WASM_OPT_V/bin/wasm-opt" "/usr/local/bin/."
 
--- .devcontainer/library-scripts/npm-dependencies.sh
+++ .devcontainer/library-scripts/npm-dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-npm install -g wasm-pack@0.10.1
+npm install -g wasm-pack@0.13.1
