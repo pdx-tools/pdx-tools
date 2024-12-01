627bb76609792ebcb0d07596a6d94e0ba24f99e1
Nick Babcock <nbabcock19@hotmail.com>
2024-12-01 21:23:43 +0000

    Structure s3 closer to r2 example

--- src/app/app/routes/api.saves.$saveId.file.ts
+++ src/app/app/routes/api.saves.$saveId.file.ts
@@ -1,3 +1,4 @@
+import { log } from "@/server-lib/logging";
 import { pdxCloudflareS3, pdxS3 } from "@/server-lib/s3";
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
 import { z } from "zod";
@@ -6,27 +7,31 @@ const saveSchema = z.object({ saveId: z.string() });
 export async function loader({ request, params, context }: LoaderFunctionArgs) {
   const save = saveSchema.parse(params);
   const s3 = pdxS3(pdxCloudflareS3({ context }));
-  const url = s3.url(s3.keys.save(save.saveId));
 
   // Need to manually cache save files in cloudflare caches to get hits
   // https://community.cloudflare.com/t/fetch-response-shows-cf-cache-status-dynamic-even-with-cacheeverything-true/299979
+  // Based on: https://developers.cloudflare.com/r2/examples/cache-api/
+  const url = new URL(request.url);
+  const cacheKey = new Request(url.toString(), request);
+
   const cache =
     "default" in context.cloudflare.caches
       ? context.cloudflare.caches.default
       : await context.cloudflare.caches.open("pdx-cache");
 
-  const cached = await cache.match(url);
-  if (cached) {
-    return cached;
+  let response = await cache.match(cacheKey);
+  if (response) {
+    log.info({ msg: "cache hit", key: save.saveId });
+    return response;
   }
 
-  const resp = await s3.fetchOk(url, {
+  log.info({ msg: "cache miss", key: save.saveId });
+  response = await s3.fetchOk(s3.keys.save(save.saveId), {
     headers: s3.headers(request.headers),
   });
 
   // We clone the response so that we can use the body more than once (one for
   // the return and the other for the cache).
-  const cachedResp = resp.clone();
-  context.cloudflare.ctx.waitUntil(cache.put(url, cachedResp));
-  return resp;
+  context.cloudflare.ctx.waitUntil(cache.put(cacheKey, response.clone()));
+  return response;
 }
