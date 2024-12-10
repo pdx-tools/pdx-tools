7a42e8ba6d4bb57e13d9f393f85072b555fc9adc
Nick Babcock <nbabcock19@hotmail.com>
2024-12-10 16:12:08 +0000

    Use remix route config in anticipation of react router v7

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -27,6 +27,7 @@ export default defineConfig(({ mode }) => ({
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+        v3_routeConfig: true,
       },
     }),
     tsconfigPaths(),
