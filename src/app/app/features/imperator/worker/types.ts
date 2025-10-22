4c93540ea7449d1359d97467870f586927c0cbc5
Nick Babcock <nbabcock19@hotmail.com>
2025-10-22 15:39:12 +0000

    Fix worker types and local dev server
    For some reason, the vite dev server update is borked by the worker type
    (it wanted to import comlink onto the server side rendering). Updating
    the other games to match EU4, fixed it.

--- src/app/app/features/imperator/worker/types.ts
+++ src/app/app/features/imperator/worker/types.ts
@@ -1,5 +1,10 @@
+import type { Remote } from "comlink";
+
 export interface ImperatorMetadata {
   date: string;
   version: string;
   isMeltable: boolean;
 }
+
+export type ImperatorWorkerModule = typeof import("./module");
+export type ImperatorWorker = Remote<ImperatorWorkerModule>;
