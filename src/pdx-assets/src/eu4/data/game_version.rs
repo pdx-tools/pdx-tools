c1551bc42c1b1b12d92683d0025227a8f9c05db1
Nick Babcock <nbabcock19@hotmail.com>
2026-09-19 08:10:27 -0500

    Calculate EU4 terrain from map files

--- src/pdx-assets/src/eu4/data/game_version.rs
+++ src/pdx-assets/src/eu4/data/game_version.rs
@@ -1,7 +1,7 @@
 use crate::FileProvider;
 use anyhow::{Context, Result};
 
-pub fn extract_game_version<P: FileProvider>(provider: &P) -> Result<String> {
+pub fn extract_game_version<P: FileProvider + ?Sized>(provider: &P) -> Result<String> {
     let raw_version =
         crate::launcher::raw_version(provider)?.context("unable to read launcher-settings.json")?;
     let major_minor: Vec<_> = raw_version.split('.').take(2).collect();
